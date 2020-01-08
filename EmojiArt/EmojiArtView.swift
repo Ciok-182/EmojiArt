6af35f57d5913e391de045e6f0c477c322ca8f17
Jorge Encinas <jorge.encinas@devant.com>
2020-01-07 18:08:31 -0600

    dropInteraction over the EmojiArtView

--- EmojiArt/EmojiArtView.swift
+++ EmojiArt/EmojiArtView.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class EmojiArtView: UIView {
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    private func setup(){
+        addInteraction(UIDropInteraction(delegate: self))
+    }
+    
     var backgroundImage : UIImage?{
         didSet{
             setNeedsDisplay()
@@ -21,4 +35,34 @@ class EmojiArtView: UIView {
     }
     
     
+}
+
+extension EmojiArtView : UIDropInteractionDelegate{
+    
+    func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
+        return session.canLoadObjects(ofClass: NSAttributedString.self)
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
+        return UIDropProposal(operation: .copy)
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+
+        session.loadObjects(ofClass: NSAttributedString.self, completion: { providers in
+            let dropPoint = session.location(in: self)
+            for attributedString in providers as? [NSAttributedString] ?? [] {
+                self.addLabel(with: attributedString, centeredAt: dropPoint)
+            }
+        })
+    }
+    
+    private func addLabel(with attributedString: NSAttributedString, centeredAt point: CGPoint) {
+        let label = UILabel()
+        label.backgroundColor = .clear
+        label.attributedText = attributedString
+        label.sizeToFit()
+        label.center = point
+        addSubview(label)
+    }
 }
